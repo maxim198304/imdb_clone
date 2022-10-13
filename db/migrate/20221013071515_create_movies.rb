271e28f257afbd6e269fd361c4d820310943b866
Maxim <maxim.khodiy@gmail.com>
2022-10-13 15:01:02 +0300

    Add movie scaffold and users

---
+++ db/migrate/20221013071515_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.string :rating
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
