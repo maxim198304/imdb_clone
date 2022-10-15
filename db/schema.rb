492dc6cf89be15db85ea56e2c308f1b1f5c72378
Maxim <maxim.khodiy@gmail.com>
2022-10-15 11:29:09 +0300

    Review scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_14_105756) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_15_072001) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_14_105756) do
     t.text "description"
     t.string "rating"
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
