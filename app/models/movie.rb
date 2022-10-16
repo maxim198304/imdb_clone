9564d42eccbc88d46ebc0003d13ff29dd6a1447f
Maxim <maxim.khodiy@gmail.com>
2022-10-16 12:51:18 +0300

    Add_movie_id_to_reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :user
-  has_many :reviews, dependent: :destroy
-  has_many :users, through: :reviews
+  has_many :reviews
+
   has_one_attached :image, :dependent => :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_072001) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_15_083050) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_072001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
